fc1e74550ca9dbe2eb477fa77815a018fb0ebd98
Samson Taylor <sltdev@protonmail.com>
2020-03-27 00:57:24 -0500

    added message Checking the weather.... to client side

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,14 @@ const p1 = document.querySelector('.p1');
 const p2 = document.querySelector('.p2');
 
 form.addEventListener('submit', (e)=>{
-    
+    p1.textContent = "Checking the weather...";
     const userInput = search.value;
     fetch(`/weather?search=${encodeURIComponent(userInput)}`).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 return p1.textContent = data.error;
             }
-
+            
             p1.textContent = `Today in ${data.destination}, the temperature is ${data.temp}`;
             p2.textContent = `${data.summary}`;
         });
